33dfed49db4b6b7a8b90fcbdfe543564084eb1aa
Lars Mogren <lars.mogren@rps.police.se>
2014-12-11 16:06:06 +0100

    Added banner and Application.

---
+++ sherlock-api/src/main/java/sherlock/api/SherlockApi.java
@@ -0,0 +1,14 @@
+package sherlock.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class SherlockApi {
+    public static void main(String[] args) {
+        SpringApplication.run(SherlockApi.class, args);
+    }
+}
